07878b7478f61763a58f2347eb2af5b8e7d36454
junnyboi <77535444+junnyboi1998@users.noreply.github.com>
2021-07-10 18:54:11 +1000

    add hover then change colour to grey

--- script.js
+++ script.js
@@ -1,53 +1,42 @@
 "use strict";
 
 const gridContainer = document.querySelector(".grid-container");
+let startingGridSize = 16;
+
+// create Div element under container
+function addDiv() {
+  const newDiv = document.createElement("div");
+  gridContainer.appendChild(newDiv).classList.add("grid-container-items");
+}
 
 // function createGrid()
 function createGrid(size) {
   if (size > 64) {
-    size = prompt("Enter new size");
+    size = Number(prompt("Enter new size"));
   }
   for (let i = 0; i < size ** 2; i++) {
-    function addDiv() {
-      const newDiv = document.createElement("div");
-      gridContainer.appendChild(newDiv).classList.add("grid-container-items");
-    }
     addDiv();
+    singleGridColor();
     gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
   }
 }
 
-// function prompt ()
-createGrid(prompt("Enter new size"));
+// hover then change colour
+function singleGridColor() {
+  const singleGrid = document.querySelectorAll(".grid-container-items");
+  singleGrid.forEach((i) => {
+    i.addEventListener("mouseover", function () {
+      i.setAttribute("style", "background:grey");
+    });
+  });
+}
 
-// function hover and change color
-// const singleGrid = document.querySelectorAll(".grid-container-items");
-// singleGrid.forEach((i) => {
-//   i.addEventListener("mouseover", function () {
-//     i.setAttribute("style", "background: grey");
+// Click Button
+const button = document.querySelector(".button");
+console.log(button);
+button.addEventListener("click", function () {
+  startingGridSize = Number(prompt("Enter New Number"));
+});
 
-// // create 16x16 grid of square divs
-// for (let i = 0; i < 16 ** 2; i++) {
-//   addDiv();
-// }
-
-// create new Etch-a-Sketch Grid
-// const singleGrid = document.querySelectorAll(".grid-container-items");
-// const button = document.querySelector(".button");
-// button.addEventListener("click", function () {
-//   // remove all singleGrid div
-//   for (let i = 0; i < 16 ** 2; i++) {
-//     gridContainer.removeChild(singleGrid[i]);
-//   }
-
-//   // prompt for new number
-//   let newDimension = Number(prompt("Enter Number"));
-
-//   // create new Div with new Numbers
-//   for (let i = 0; i < newDimension ** 2; i++) {
-//     addDiv();
-//     gridContainer.style.gridTemplateColumns = `repeat(${newDimension}, 1fr)`;
-//     gridContainer.style.gridTemplateRows = `repeat(${newDimension}, 1fr)`;
-//   }
-// });
+createGrid(16);
